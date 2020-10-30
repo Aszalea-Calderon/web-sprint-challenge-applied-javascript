2f7c4796ffb7898f432803c0f47749f128adcf44
Aszalea Calderon <aszalea_calderon@yahoo.com>
2020-10-30 08:28:19 -0700

    header done

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,30 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+  const headerContainer = document.querySelector(".header-container");
+  const headerDiv = document.createElement("div");
+  const dateSpan = document.createElement("span");
+  const lambdaTimesH1 = document.createElement("h1");
+  const tempSpan = document.createElement("span");
+
+  //appending
+  headerContainer.append(headerDiv);
+  headerDiv.append(dateSpan);
+  headerDiv.append(lambdaTimesH1);
+  headerDiv.append(tempSpan);
+
+  //classes
+  headerDiv.classList.add("header");
+  dateSpan.classList.add("date");
+  tempSpan.classList.add("temp");
+
+  //text Content
+  dateSpan.innerHTML = "MARCH 28, 2020";
+  lambdaTimesH1.innerHTML = "Lambda Times";
+  tempSpan.innerHTML = "98°";
+
+  return headerDiv;
+}
+const head = Header("");
+console.log(head);
